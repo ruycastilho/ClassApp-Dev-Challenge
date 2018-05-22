0ce8f06e5600317c698363e9756ebca85df38ade
rRuy Castilho Barrichelo <ruy.castilho26@gmail.com>
2018-05-22 14:38:17 -0300

    Initial commit. Using React+NodeJS+Express

---
+++ classapp-dev-challenge/client/src/main.js
@@ -0,0 +1,9 @@
+// ClassApp DevChallenge
+// Ruy Castilho Barrichelo
+// Used Sources:
+//  https://gist.github.com/lucas-brito/84a77f08115ae4b9b034c010ff2a2ab4
+//  https://www.w3schools.com/nodejs/nodejs_modules.asp
+
+
+
+
