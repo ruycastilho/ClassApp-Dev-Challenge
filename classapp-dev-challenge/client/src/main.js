ef165cb8ccca713e175c5620bc139c00a053ef1d
rRuy Castilho Barrichelo <ruy.castilho26@gmail.com>
2018-05-22 14:56:47 -0300

    Fixed npm commands

--- classapp-dev-challenge/client/src/main.js
+++ classapp-dev-challenge/client/src/main.js
@@ -1,9 +1,11 @@
-// ClassApp DevChallenge
+// ClassApp DevChallenge - https://classapp-dev-challenge.herokuapp.com/
 // Ruy Castilho Barrichelo
 // Used Sources:
 //  https://gist.github.com/lucas-brito/84a77f08115ae4b9b034c010ff2a2ab4
 //  https://www.w3schools.com/nodejs/nodejs_modules.asp
-
+//  https://medium.freecodecamp.org/how-to-make-create-react-app-work-with-a-node-backend-api-7c5c48acb1b0
+//  https://codeburst.io/build-simple-medium-com-on-node-js-and-react-js-a278c5192f47
+//  https://www.fullstackreact.com/articles/deploying-a-react-app-with-a-server/
 
 
 
