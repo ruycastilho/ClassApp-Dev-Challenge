1023592f04552a3952af6f93ae72e53a2af24bbd
rRuy Castilho Barrichelo <ruy.castilho26@gmail.com>
2018-05-22 15:08:09 -0300

    Removed node_modules folder from git

--- classapp-dev-challenge/client/src/main.js
+++ classapp-dev-challenge/client/src/main.js
@@ -1,10 +1,13 @@
-// ClassApp DevChallenge - https://classapp-dev-challenge.herokuapp.com/
-// Ruy Castilho Barrichelo
-// Used Sources:
+// ClassApp DevChallenge
+// Available at: https://classapp-dev-challenge.herokuapp.com/
+// Author: Ruy Castilho Barrichelo, github.com/ruycastilho
+// Sources:
 //  https://gist.github.com/lucas-brito/84a77f08115ae4b9b034c010ff2a2ab4
 //  https://www.w3schools.com/nodejs/nodejs_modules.asp
 //  https://medium.freecodecamp.org/how-to-make-create-react-app-work-with-a-node-backend-api-7c5c48acb1b0
 //  https://codeburst.io/build-simple-medium-com-on-node-js-and-react-js-a278c5192f47
+//  https://www.fullstackreact.com/articles/deploying-a-react-app-with-a-server/
+//  https://shift.infinite.red/npm-vs-yarn-cheat-sheet-8755b092e5cc
 //  https://www.fullstackreact.com/articles/deploying-a-react-app-with-a-server/
 
 
