21460d28322305e8b29db33c5bcaf7c431d738b2
rRuy Castilho Barrichelo <ruy.castilho26@gmail.com>
2018-05-28 13:52:41 -0300

    Cleaned and commented code

--- parser/parser.js
+++ parser/parser.js
@@ -1,72 +1,88 @@
-// var csv = require('csv-parser');
-// var csv = require("fast-csv");
+// ClassApp DevChallenge (Parser Only)
+// Available at: https://classapp-dev-challenge.herokuapp.com/
+// Challenge: https://gist.github.com/lucas-brito/84a77f08115ae4b9b034c010ff2a2ab4
+// Author: Ruy Castilho Barrichelo, github.com/ruycastilho
+
+// Packages Used
+
+// CSV-Parse : .csv file input and initial parsing
 var parse = require('csv-parse');
 var transform = require('stream-transform');
+// Filestream : Working with file I/O
 var fs = require("fs");
-var path = require("path");
+// Lodash : "Working with arrays, numbers, objects, strings, etc.""
 var _ = require('lodash');
+// Google-Libphonebumber : Working with phone numbers
 const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 const PNF = require('google-libphonenumber').PhoneNumberFormat;
+// Validator : Email validation
 var validator = require('validator');
 
+// Data Structures
+// Constructors
+
+// User
 function User(fullname, eid, classes, addresses, invisible, see_all) {
     this.fullname = fullname;
     this.eid = eid;
     this.classes = classes;
-    this.addresses= addresses;
+    this.addresses= addresses;  // Array of Address
     this.invisible = invisible;
     this.see_all = see_all;
 }
 
+// Address
 function Address(type, tags, address) {
-    this.type = type;
+    this.type = type;           // Phone or Email
     this.tags = tags;
     this.address = address;
 }
 
+// Column (Stores header data, separates it into type(title) and tags)
 function Column(type, tags) {
     this.type = type;
     this.tags = tags;
 
 }
 
-var output = [];
-var columns = [];
-var mainHeaderList;
+// Arrays to store main input and output data
+var output = [];                // Array of User objects, to be transformed to JSON format
+var header = [];               // Array of Column objects that stores .csv header data
 
+// .csv Input and Initial parsing
 fs.readFile('input.csv', function (err, fileData) {
+    
+    // Reads data from 'input.csv' and stores it in the array named 'rows'
     parse(fileData, function(err, rows) {
-        // console.log(rows);
 
+        // Separates first row as a header and separates each column as an element of an array
         var headerList = _.chunk(_.head(rows), 1);
-        // console.log(headerList);
 
+        // Drops header from 'rows' so that it only contais user information
         var data_rows = _.drop(rows);
-        // console.log(data_rows);
 
+        // Auxiliar variables for parsing header
         var tokenized;
-        var tags;
-        var type;
-        var body;
+        var tags = [];
+        var type;   
 
+        // Header Parsing
         for(i=0; i < headerList.length ; i++) {
-            tags = [];
-            tokenized = headerList[i].toString().replace(/,/g, '').split(" ");
-            type = _.head(tokenized);
-            body = _.drop(tokenized);
 
-            for(k=0; k < body.length; k++) {
-                // console.log("hey: " + body[k]);
+            tags.splice(0, tags.length);                                        // Clearing array
+            tokenized = headerList[i].toString().replace(/,/g, '').split(" ");  // Tokenizes entry
+            type = _.head(tokenized);                                           // Selects first token as header type(title)
+            tags = _.drop(tokenized);                                           // Remaining tokens are tags
 
-                tags.push(body[k]);
 
-            }
-
-            columns.push(new Column(type, tags));
+            // Stores new header element in an array 
+            header.push(new Column(type, tags.slice()));
         }
 
-        // console.log(columns);
-        // var found_user;
+        console.log(header);
+        // User information parsing
+
+        // Auxiliar variables for parsing
         var user_index,
             name,
             id,
@@ -82,26 +98,39 @@ fs.readFile('input.csv', function (err, fileData) {
             phone_number,
             parsed_number;
 
+        // Runs through each row of user data from input file
         for(i=0; i < data_rows.length ; i++) {
-            // console.log(data_rows[i]);
+
+            // Separates each column entry from a row
             data_list = _.chunk(data_rows[i], 1);
+
+            // Clearing arrays
             classes.splice(0, classes.length);
             addresses.splice(0, addresses.length);
             tokenized_classes.splice(0, tokenized_classes.length);
             tokenized_emails.splice(0, tokenized_emails.length);
 
+            // Runs through each row entrt
             for(j=0; j < data_rows[i].length ; j++) {
 
-                switch ( columns[j].type ) {
-
+                // Switch to determine actions to be performed depending on the type of entry (based on the 'header' array)
+                switch ( header[j].type ) {
+                
+                    // If entry corresponds to a name, just stores it
                     case 'fullname':
                         name = data_list[j].toString();
                         break;
+
+                    // If entry corresponds to an eid, just stores it
                     case 'eid':
                         id = data_list[j].toString();
                         break;
+
+                    // If entry corresponds to 'invisible':
                     case 'invisible':
                         invisible_input = data_list[j].toString();
+
+                        // Considers empty entry, "0" and "no" as a 'false' value
                         if (invisible_input == "" || invisible_input == "0" || invisible_input == "no") {
                             invisible = false;
                         }
@@ -110,8 +139,12 @@ fs.readFile('input.csv', function (err, fileData) {
 
                         }
                         break;
+
+                    // If entry corresponds to 'see_all':
                     case 'see_all':
                         see_all_input = data_list[j].toString();
+
+                        // Considers empty entry, "0" and "no" as a 'false' value
                         if (see_all_input == "" || see_all_input == "0" || see_all_input == "no") {
                             see_all = false;
                         }
@@ -120,12 +153,15 @@ fs.readFile('input.csv', function (err, fileData) {
 
                         }
                         break;
+                    
+                    // If entry corresponds to 'class':
                     case 'class':
-                        tokenized_classes = _.chunk(data_list[j].toString().replace('/', ',').split(","), 1);
-                        // tokenized_classes.map(function(tok_class) { return tok_class.toString().trim() });
 
+                        // Replaces '/' with ',' to obtain a consistent pattern, and tokenizes string with commas as delimiters
+                        tokenized_classes = _.chunk(data_list[j].toString().replace('/', ',').split(","), 1);
+
+                        // Adds new value to an array, if not empty
                         for(k=0; k < tokenized_classes.length; k++) {
-                            // console.log("class to add: " + tokenized_classes[k]);
                             if (tokenized_classes[k].toString() != "") {
                                 classes.push(tokenized_classes[k].toString().trim());
 
@@ -134,10 +170,13 @@ fs.readFile('input.csv', function (err, fileData) {
                         }                 
  
                         break;
+
+                    // If entry corresponds to 'phone':
                     case 'phone':
+                        // Initial trimming
                         phone_number = data_list[j].toString().trim();
-                        // console.log("phone: " + phone_number);
                         
+                        // Attempts to parse input as a phone number, if it fails (error thrown, just skips entry)
                         try {
                             parsed_number = phoneUtil.parse(phone_number, 'BR');
 
@@ -146,131 +185,81 @@ fs.readFile('input.csv', function (err, fileData) {
                             break;
                         }
 
+                        // If parsing is successfull and the parsed number is valid:
                         if ( phoneUtil.isValidNumber(parsed_number) ) {
-                            // console.log("parsed: " + phoneUtil.format(parsed_number, PNF.E164).replace("+", ""));
+
+                            // Searches for a previous phone number input with same number
+                            // If it is found, just adds new tags, if not, adds new Address object to an array
                             var index = addresses.findIndex( function (addr) {
                                 return addr.address == phone_number && addr.type == 'phone'
                             });
                             if (index != -1) {
-                                addresses[index].tags.push.apply(addresses[index].tags, columns[j].tags.spice());
+                                addresses[index].tags.push.apply(addresses[index].tags, header[j].tags.slice());
                             }
                             else {
-                                addresses.push(new Address(columns[j].type, columns[j].tags.slice(), phoneUtil.format(parsed_number, PNF.E164).replace("+", "")));
+                                addresses.push(new Address(header[j].type, header[j].tags.slice(), phoneUtil.format(parsed_number, PNF.E164).replace("+", "")));
                             }
 
                         }
                         break;
-                        
-                    case 'email':
-                        tokenized_emails = _.chunk(data_list[j].toString().replace('/', ',').split(","), 1);
-                        tokenized_emails.map(function(tok_email) { return tok_email.toString().trim() });
-                        // for(k=0; k < tokenized_emails.length; k++) {
-                        //     console.log("trimmed:'" + tokenized_emails[k].toString() + "'");
-                        // }
 
+                    // If entry corresponds to 'email':
+                    case 'email':
+                        // Replaces '/' with ',' to obtain a consistent pattern, and tokenizes string with commas as delimiters
+                        tokenized_emails = _.chunk(data_list[j].toString().replace('/', ',').split(","), 1);
+                        // Trims every element
+                        tokenized_emails.map(function(tok_email) { return tok_email.toString().trim() });
+           
+                        // Checks which email inputs are valid
                         for(k=0; k < tokenized_emails.length; k++) {
                             if (validator.isEmail(tokenized_emails[k].toString()) ) {
-                                // console.log("email to add: " + tokenized_emails[k].toString());
-                                // console.log("column tags:" + columns[j].tags);
-
-                                // var test = addresses.findIndex( function (addr) {
-                                //     return addr.address == "johndoemae1@gmail.com";
-
-                                    
-                                // });
-                                // console.log("mae tags:" + addresses[test]);
-
+             
+                                // If valid:
+                                // Searches for a previous email input with same address
+                                // If it is found, just adds new tags, if not, adds new Address object to an array
                                 var index = addresses.findIndex( function (addr) {
                                     return addr.address == tokenized_emails[k] && addr.type == 'email'
                                 });
+
                                 if (index != -1) {
-                                    // console.log("curr tags:" + addresses[index].tags + '\ntags:' + columns[j].tags.slice());
-                                    addresses[index].tags.push.apply(addresses[index].tags, columns[j].tags.slice());
-                                    // console.log("curr tags:" + addresses[index].tags);
+                                    addresses[index].tags.push.apply(addresses[index].tags, header[j].tags.slice());
 
                                 }
                                 else {
-                
-                                    addresses.push(new Address(columns[j].type, columns[j].tags.slice(), tokenized_emails[k].toString()));
+                                    addresses.push(new Address(header[j].type, header[j].tags.slice(), tokenized_emails[k].toString()));
         
                                 }
                             }
                         }
-                        // tokenized_emails.forEach(addresses.push(new Address(columns[j].type, columns[j].tags, ));
                         break;
-                    
                 }
 
             }
-            // for(k=0; k < classes.length; k++) {
-            //     console.log("classes:'" + classes[k] + "'");
 
-            // }
-            // console.log("\n");
-            // console.log("\naddresses:");
-            // for(k=0; k < addresses.length; k++) {
-            //     console.log("\naddress:\ntype:'" + addresses[k].type  + "'" + "\ntags:" + addresses[k].tags + "\naddr:" + addresses[k].address );
-
-            // }
-            
-            // console.log("\n");
+            // After all parsing is done:
+            // Searches for previous user data/row input with same 'eid'
             user_index = output.findIndex( function (user) {
                 return user.eid == id;
             });
-            // console.log("'\nid:'" + id + "'");
 
+            // If it is found, update addresses, classes and boolean variables
             if (user_index != -1) {
-                // console.log("User id:'" + output[user_index].eid + "'\nid:'" + id + "'\n");
-                // for(k=0; k < classes.length; k++) {
-                //     console.log("classes:'" + classes[k] + "'");
-    
-                // }
-                // console.log("\n");
-
-                // for(k=0; k < output[user_index].classes.length; k++) {
-                //     console.log("curr classes:'" + output[user_index].classes[k] + "'");
-    
-                // }
-                // console.log("\naddresses:");
-                // for(k=0; k < addresses.length; k++) {
-                //     console.log("\naddress:\ntype:'" + addresses[k].type  + "'" + "\ntags:" + addresses[k].tags + "\naddr:" + addresses[k].address );
-    
-                // }
-                
-                // console.log("\n");
-                // console.log("\ncurr addresses:");
-                // for(k=0; k <  output[user_index].addresses.length; k++) {
-                //     console.log("\naddress:\ntype:'" +  output[user_index].addresses[k].type  + "'" + "\ntags:" + output[user_index].addresses[k].tags + "\naddr:" +  output[user_index].addresses[k].address );
-    
-                // }
+                // Extending arrays
                 output[user_index].addresses.push.apply(output[user_index].addresses, addresses.slice());
                 output[user_index].classes.push.apply(output[user_index].classes, classes.slice());
+
+                // Boolean operations (default is 'false', so if new value is 'true', it will be updated (logic OR))
                 output[user_index].invisible = output[user_index].invisible || invisible;
                 output[user_index].see_all = output[user_index].see_all ||see_all;
             }
+            // If not, pushes new User object
             else {
                 output.push(new User(name, id, classes.slice(), addresses.slice(), invisible, see_all));
             }
-            // console.log("\nData:");
-            // console.log("fullname:'" + name + "'");
-            // console.log("id:'" + id + "'");
-            // console.log("invisible:'" + invisible + "'");
-            // console.log("see_all:'" + see_all + "'\n");
-            // for(k=0; k < classes.length; k++) {
-            //     console.log("classes:'" + classes[k] + "'");
-
-            // }
-            // console.log("\naddresses:");
-            // for(k=0; k < addresses.length; k++) {
-            //     console.log("\naddress:\ntype:'" + addresses[k].type  + "'" + "\ntags:" + addresses[k].tags + "\naddr:" + addresses[k].address );
-
-            // }
-            // console.log("\n");
+     
         }
-        // var outputJSON = transform(output, function(data){
-        //     data.push(data.shift())
-        //   return data;
-        // });
+
+        // Finally, transforms array into a JSON and outputs it
         var outputJSON = JSON.stringify(output, null, 2);
         console.log(outputJSON);
     });
